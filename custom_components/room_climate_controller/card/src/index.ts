351fa6325dfaf39ef2aadaf3905c297155e4f6db
Todd Burruss <btoddb@gmail.com>
2026-06-09 07:19:28 +0000

    release v0.4.2

--- custom_components/room_climate_controller/card/src/index.ts
+++ custom_components/room_climate_controller/card/src/index.ts
@@ -12,7 +12,7 @@ window.customCards.push({
 });
 
 console.info(
-  "%c ROOM-CLIMATE-CONTROL %c v1.4.30 ",
+  "%c ROOM-CLIMATE-CONTROL %c v1.4.31 ",
   "color: white; background: #0288d1; font-weight: 700;",
   "color: #0288d1; background: white; font-weight: 700;"
 );

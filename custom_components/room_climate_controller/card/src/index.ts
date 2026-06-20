faa7d4fff979af272fd41f932e70a96da326bc38
btodd <btoddb@gmail.com>
2026-06-20 02:45:59 +0000

    release v0.4.24

--- custom_components/room_climate_controller/card/src/index.ts
+++ custom_components/room_climate_controller/card/src/index.ts
@@ -12,7 +12,7 @@ window.customCards.push({
 });
 
 console.info(
-  "%c ROOM-CLIMATE-CONTROL %c v1.4.79 ",
+  "%c ROOM-CLIMATE-CONTROL %c v1.4.80 ",
   "color: white; background: #0288d1; font-weight: 700;",
   "color: #0288d1; background: white; font-weight: 700;"
 );
